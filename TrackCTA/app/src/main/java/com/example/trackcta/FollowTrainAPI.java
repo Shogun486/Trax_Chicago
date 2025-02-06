befb15dcf455af7f2d755f2b915f13f6b80ad022
Shagun Karmacharya <72041905+Shogun486@users.noreply.github.com>
2025-02-06 17:24:39 -0600

    logs minutes until train arrival

---
+++ TrackCTA/app/src/main/java/com/example/trackcta/FollowTrainAPI.java
@@ -0,0 +1,66 @@
+package com.example.trackcta;
+
+import android.util.Log;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FollowTrainAPI
+{
+
+    private static final String API_KEY = "a64c36c7f3174612a48134c15b3568a5",
+            PRE_KEY_URL = "http://lapi.transitchicago.com/api/1.0/ttfollow.aspx?key=",
+            POST_KEY_URL1 = "&runnumber=",
+            POST_KEY_URL2 = "&outputType=JSON";
+    private static String URL;
+    private static RequestQueue queue;
+    private static VisualizerActivity visualizerActivity;
+
+
+    public static void call(VisualizerActivity visualizerActivity, int run)
+    {
+        FollowTrainAPI.visualizerActivity = visualizerActivity;
+        queue = Volley.newRequestQueue(FollowTrainAPI.visualizerActivity);
+
+        Response.Listener <JSONObject> listener = new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+
+                try
+                {
+                    JSONObject initialQuery = new JSONObject(response.toString());
+                    JSONObject ctatt = (JSONObject) initialQuery.get("ctatt");
+                    JSONArray eta = (JSONArray) ctatt.getJSONArray("eta");
+                    //JSONObject first = (JSONObject) eta.get(0);
+                    Log.d("eta", eta.toString());
+
+                } catch (JSONException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        Response.ErrorListener error = new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Log.d("ERROR", error.toString());
+            }
+        };
+        URL = PRE_KEY_URL + API_KEY + POST_KEY_URL1 + run + POST_KEY_URL2;
+        Log.d("URL", URL);
+        queue.add(new JsonObjectRequest(Request.Method.GET, URL, null, listener, error));
+
+    }
+
+
+}
--- TrackCTA/app/src/main/java/com/example/trackcta/VisualizerActivity.java
+++ TrackCTA/app/src/main/java/com/example/trackcta/VisualizerActivity.java
@@ -4,6 +4,15 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.content.Intent;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/*
+    To-implement
+ */
 
 public class VisualizerActivity extends AppCompatActivity
 {
@@ -18,9 +27,84 @@ public class VisualizerActivity extends AppCompatActivity
 
         Intent intent = getIntent();
         run = intent.getIntExtra("RUN", -1);
-        arrivalTime = intent.getStringExtra("ARRIVAL");
+
+        // Arrival Time (military)
+        arrivalTime = intent.getStringExtra("ARRIVAL").split("T")[1];
+        Log.d("arrivalTime", arrivalTime);
+
+        // Current Time (military)
+        String currentTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        Log.d("currentMilitary", currentTime);
+        convertFromMilitary(currentTime);
+
+        getMinuteDifference(currentTime, arrivalTime);
+        //FollowTrainAPI.call(this, run);
 
         Log.d("VISUALIZER", String.valueOf(run));
         Log.d("VISUALIZER", arrivalTime);
+    }
+
+
+    private static void convertFromMilitary(String time)
+    {
+        int h1 = (int) time.charAt(0) - '0', h2 = (int) time.charAt(1)- '0', hour = h1 * 10 + h2;
+        String meridiem = (hour < 12 ? "AM" : "PM");
+        StringBuilder display = new StringBuilder("");
+        hour %= 12;
+
+        if (hour == 0)
+        {
+            display.append("12");
+            for (int i = 2; i < 8; i++)
+                display.append(time.charAt(i));
+        }
+        else
+        {
+            display.append(hour);
+            for (int i = 2; i < 8; i++)
+                display.append(time.charAt(i));
+        }
+
+        display.append(" ");
+        display.append(meridiem);
+        Log.d("currentCST", display.toString());
+    }
+
+    private static void getMinuteDifference(String now, String future)
+    {
+        if(now.substring(0,2).equals("23") && future.substring(0,2).equals("00")) // traveling at midnight
+        {
+            StringBuilder sb = new StringBuilder(now);
+            sb.setCharAt(0, '1');
+            sb.setCharAt(1,'0');
+            now = sb.toString();
+
+            sb = new StringBuilder(future);
+            sb.setCharAt(0, '1');
+            sb.setCharAt(1,'1');
+            future = sb.toString();
+        }
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        Date d1, d2;
+
+        try
+        {
+            d1 = simpleDateFormat.parse(now);
+            d2 = simpleDateFormat.parse(future);
+        } catch (ParseException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        long differenceInMilliSeconds = Math.abs(d2.getTime() - d1.getTime()),
+                differenceInHours = (differenceInMilliSeconds / (60 * 60 * 1000)) % 24,
+                differenceInMinutes = (differenceInMilliSeconds / (60 * 1000)) % 60,
+                differenceInSeconds = (differenceInMilliSeconds / 1000) % 60;
+
+        Log.d("DIFFERENCE",
+                "Difference is " + differenceInHours + " hours "
+                        + differenceInMinutes + " minutes "
+                        + differenceInSeconds + " seconds. \n" + now + "\n" + future);
     }
 }
